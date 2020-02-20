98cf19e526f9520ec10d9440a6a254a74cadf4ce
Wrightl71559 <60583421+Wrightl71559@users.noreply.github.com>
2020-02-21 10:53:52 +1300

    full game

--- 06_Lucky_Uniccorn.py
+++ 06_Lucky_Uniccorn.py
@@ -23,30 +23,43 @@ def intcheck(question, low, high):
 # Ask user how much they want to play with (min $1, max $10)
 balance = intcheck("How much money would you like to play with? ", 1, 10)
 
-# Tokens list includes 10 items to prevent too many unicorns being chosen
-tokens = ["horse", "horse", "horse",
-          "zebra", "zebra", "zebra",
-          "donkey", "donkey", "donkey", "unicorn"]
+keep_going = ""
+while keep_going == "":
 
-# Randomly choose a token from our list above
-token = random.choice(tokens)
-print()
-print("you got a {}.".format(token))
+    # Tokens list includes 10 items to prevent too many unicorns being chosen
+    tokens = ["horse", "horse", "horse",
+              "zebra", "zebra", "zebra",
+              "donkey", "donkey", "donkey", "unicorn"]
 
-# Adjust toal correctly for a given token
-if token == "unicorn":
-    balance += 5    # wins $5
-    feedback = "Congratulations you won $5.00"
-elif token == "donkey":
-    balance -= 1    # loses $1
-    feedback = "Sorry, you did not win anything this round"
-else:
-    balance -= 0.5  # wins / loses 50c
-    feedback = "You've won 50c"
+    # Randomly choose a token from our list above
+    token = random.choice(tokens)
+    print()
+    print("you got a {}.".format(token))
 
-print()
-print (feedback)
-print("you have ${:.2f} to play with".format(balance))
+    # Adjust toal correctly for a given token
+    if token == "unicorn":
+        balance += 5    # wins $5
+        feedback = "Congratulations you won $5.00"
+    elif token == "donkey":
+        balance -= 1    # loses $1
+        feedback = "Sorry, you did not win anything this round"
+    else:
+        balance -= 0.5  # wins / loses 50c
+        feedback = "You've won 50c"
+
+    print()
+    print (feedback)
+    print("you have ${:.2f} to play with".format(balance))
+
+    # If user has enough money to play, ask if they want to play again
+    if balance < 1:
+        print("Sorry, you don't have enough money to continue. Game over")
+        keep_going = "end"
+    else:
+        keep_going = input("Press <enter> to play again or any key to quit")
+
+# Farewell user at end of game.
+print("Thank you for playing :)")
 
 
 
